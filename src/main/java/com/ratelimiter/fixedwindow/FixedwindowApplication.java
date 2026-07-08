d28633ba7c2a5a52497851adfb99cde1f40580d9
Suryapag <suryapa9092@gmail.com>
2026-07-08 20:32:03 +0530

    Project setup completed

---
+++ src/main/java/com/ratelimiter/fixedwindow/FixedwindowApplication.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.fixedwindow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedwindowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedwindowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratelimiter/fixedwindow/FixedwindowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.fixedwindow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedwindowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
